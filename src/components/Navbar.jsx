0167b0dee5367c93c018f4cb3071c34942ab0fe7
mangionejt <109229232+mangionejt@users.noreply.github.com>
2022-10-13 11:56:34 -0300

    Add files via upload

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import { Stack } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { logo } from '../utils/constants';
+import SearchBar from './SearchBar';
+
+const Navbar = () => (
+    <div>
+      <Stack
+       direction="row"
+       alignItems="center" 
+       p={2} 
+       sx={{ position: 'sticky', background: '#000', top: 0, justifyContent: 'space-between'}}
+      >
+        <Link  to="/" style={{ display:"flex", alignItems: 'center', marginLeft: '20px' }}>
+          <img src={ logo } alt="logo" height={65} />
+        </Link>
+        <SearchBar></SearchBar>
+      </Stack>
+    </div>
+  )
+
+
+export default Navbar
